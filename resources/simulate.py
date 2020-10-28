a2833ad4e13201cfacb8f131ceaa839c91aaa919
FloriaPeng <46766400+FloriaPeng@users.noreply.github.com>
2020-10-28 15:34:36 -0400

    Added get method to simulate for fetching.

--- resources/simulate.py
+++ resources/simulate.py
@@ -6,10 +6,16 @@ CIRCUIT_RECEIVED = "{} received"
 
 class Simulate(Resource):
 
+    circuit_json = {}
+
+    @classmethod
+    def get(cls, name: str):
+        return cls.circuit_json, 200
+
     @classmethod
     def post(cls, name: str):
-        circuit_json = request.get_json()  # circuit elements
-        circuit_json["name"] = name  # the item name is not in the request body, instead, it is in the url
+        cls.circuit_json = request.get_json()  # circuit elements
+        cls.circuit_json["name"] = name  # the item name is not in the request body, instead, it is in the url
 
         # return {"message": CIRCUIT_RECEIVED.format(name)}, 201
-        return circuit_json, 201
+        return cls.circuit_json, 201
